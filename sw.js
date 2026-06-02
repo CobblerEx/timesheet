9bbe7d66b71e93e44196d6d552ec930622926e6d
CobblerEx <141290116+CobblerEx@users.noreply.github.com>
2026-06-02 14:19:43 -0400

    Offline version

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'timesheet-1780355238344';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Serve root path from cached HTML
+  if (url.pathname === '/' || url.pathname === '') {
+    e.respondWith(caches.match('./index.html'));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request)
+      .then(hit => hit || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
